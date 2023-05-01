ce45b802e99cbd44bd3c0a99cf49c1bb131b9365
Erik Richard <erichard413@outlook.com>
2023-05-01 14:27:25 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../stylesheets/header.css';
+import { SocialIcon } from 'react-social-icons';
+
+const Header = ()=> {
+    return (
+        <div className="header">
+            <div className="logo-div">
+                <span id="logo">ER</span>
+            </div>
+            <div className="name-div">
+                <ul>
+                    <li>Hi, my name is:</li>
+                    <li>Erik Richard</li>
+                    <li>Aspiring web developer</li>
+                </ul>
+            </div>
+            <div className="contact-div">
+                <div>
+                    <p>Connect: 
+                    <SocialIcon url="https://www.linkedin.com/in/erik-richard-519a0b21/" bgColor="white" style={{height: 30, width: 30, marginLeft: 5}} />
+                    <SocialIcon url="https://www.instagram.com/erikrichard/" bgColor="white" style={{height: 30, width: 30, marginLeft: 5}} />
+                    <SocialIcon url="https://github.com/erichard413" bgColor="white" style={{height: 30, width: 30, marginLeft: 5}} />
+                    <SocialIcon url="https://twitter.com/erichard413" bgColor="white" style={{height: 30, width: 30, marginLeft: 5}} />
+                    <SocialIcon url="mailto:erichard413@outlook.com" bgColor="white" style={{height: 30, width: 30, marginLeft: 5}} />
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
